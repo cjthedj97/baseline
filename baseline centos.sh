9d619da191074b70e6c89bb76edab9cedc38ff93
CJ S <csaath01@baker.edu>
2017-10-31 15:35:40 -0400

    added root checking

--- baseline centos.sh
+++ baseline centos.sh
@@ -3,3 +3,9 @@
 ## Written by cjthedj97 on Github
 
 ## The Start of the Script
+
+## Checking to see if script was ran as root or with sudo privileges
+if [ $(id -u) != 0 ]; then
+    echo "You're not root. please run as root"
+    exit
+fi
