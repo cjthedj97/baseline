8969b67fa41d7d44bb046f926034557a5ee55c44
CJ S <csaath01@baker.edu>
2017-10-31 15:29:42 -0400

    Added more to the script

--- baseline debian.sh
+++ baseline debian.sh
@@ -3,3 +3,20 @@
 ## Written by cjthedj97 on Github
 
 ## The Start of the Script
+
+## Checking to see if script was ran as root or with sudo privileges
+if [ $(id -u) != 0 ]; then
+    echo "You're not root. please run as root"
+    exit
+fi
+
+## Checks the /etc/apt/sources.list and ask if it is correct
+echo "Please verify that the source list is correct"
+cat /etc/apt/sources.list
+echo "is this correct"
+read a
+if [[ $a == "Y" || $a == "Y" ]]; then
+        echo "you entered Y"
+else
+        echo "You entered N or an incorrct response"
+fi
